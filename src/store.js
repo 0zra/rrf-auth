0fe5debdec9a108a02fb98f096239878cec776bb
ozra <petar.ozretic@mail.com>
2018-09-06 12:44:03 +0200

    Implement basic login

--- src/store.js
+++ src/store.js
@@ -47,8 +47,8 @@ const initialState = {};
 const store = createStoreWithFirebase(
   rootReducer,
   initialState,
-  compose(reactReduxFirebase(firebase))
-  // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(reactReduxFirebase(firebase)),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
