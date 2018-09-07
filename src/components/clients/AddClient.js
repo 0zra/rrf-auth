6b463f299f18958a24f9c3ad0e936e0b58779b87
ozra <petar.ozretic@mail.com>
2018-09-07 06:27:48 +0200

    Implement settings functionality

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-//import { compose } from "redux";
-//import { connect } from "react-redux";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 
 class AddClient extends React.Component {
@@ -33,6 +33,7 @@ class AddClient extends React.Component {
   };
 
   render() {
+    const { disableBalanceOnAdd } = this.props.settings;
     return (
       <div>
         <div className="row">
@@ -101,6 +102,7 @@ class AddClient extends React.Component {
                   name="balance"
                   onChange={this.onChange}
                   value={this.state.balance}
+                  disabled={disableBalanceOnAdd}
                 />
               </div>
               <input
@@ -117,7 +119,13 @@ class AddClient extends React.Component {
 }
 
 AddClient.propTypes = {
-  firestore: PropTypes.object.isRequired
+  firestore: PropTypes.object.isRequired,
+  settings: PropTypes.object.isRequired
 };
 
-export default firestoreConnect()(AddClient);
+export default compose(
+  firestoreConnect(),
+  connect((state, props) => ({
+    settings: state.settings
+  }))
+)(AddClient);
